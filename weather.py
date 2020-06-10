31602124b9de3b37292bdc6004fa389ce4ea6627
QA engineer <kotarakh@gmail.com>
2020-06-10 04:51:52 +0300

    Reformatted code

--- weather.py
+++ weather.py
@@ -1,17 +1,17 @@
-
-
 DB = {
-    "Харьков":[27,"Солнечно"],
-    "Киев":[25,"Облачно"],
-    "Лондон":[22,"Ливни"],
-    "Нью-Йорк":[28,"Временами грозы"]
+    "Харьков": [27, "Солнечно"],
+    "Киев": [25, "Облачно"],
+    "Лондон": [22, "Ливни"],
+    "Нью-Йорк": [28, "Временами грозы"]
 }
+
 
 class Weather_db:
 
     @staticmethod
     def update_info(key, temp=None, description=None):
-        DB.update({key:[temp,description]})
+        DB.update({key: [temp, description]})
+
 
 class City:
     def __init__(self, city):
@@ -20,9 +20,10 @@ class City:
             self.__weather = DB[city]
         except:
             print("Invalid value or this city is not tracking yet")
+
     def show_weather(self):
         print("Температура: {0}\n" \
-                "В общем: {1}\n".format(self.__weather[0], self.__weather[1]))
+              "В общем: {1}\n".format(self.__weather[0], self.__weather[1]))
 
     @staticmethod
     def set_city():
